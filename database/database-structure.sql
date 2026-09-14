141a7ab0560f23abc4965b086dc7b9428484907d
Yrla137 <julia.bjorling@hotmail.com>
2026-09-14 15:17:00 +0200

    feat: add database configuration and update schema for user roles and payments

--- database/database-structure.sql
+++ database/database-structure.sql
@@ -8,11 +8,23 @@ CREATE TABLE users (
     last_name VARCHAR(100) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
     password_hash VARCHAR(255) NOT NULL,
+    role ENUM('member', 'administrator') NOT NULL DEFAULT 'member',
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+-- PLAN TABLE
+
+-- FEATURES TABLE
+
 -- PAYMENTS TABLE
+CREATE TABLE payments (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    subscription_plan_id INT NOT NULL,
+    amount DECIMAL(10, 2) NOT NULL,
+    payment_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (subscription_plan_id) REFERENCES subscription_plans(id)
+);
 
--- SUBSCRIPTIONS TABLE
-
--- SUBSCRIPTION_PLANS TABLE
+-- USER PLANNER DATA TABLE
