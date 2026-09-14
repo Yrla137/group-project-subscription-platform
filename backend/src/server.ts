41acd1718ab28a5029dd42ab8518ac3e67458650
Yrla137 <julia.bjorling@hotmail.com>
2026-09-14 15:29:27 +0200

    feat: update .gitignore and add PostgreSQL dependency; implement database connection test

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,1 +1,14 @@
-console.log(":D");
+import { pool } from "./config/db";
+
+const testDatabaseConnection = async () => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+
+    console.log("Database connected!");
+    console.log(result.rows);
+  } catch (error) {
+    console.error("Database connection failed:", error);
+  }
+};
+
+testDatabaseConnection();
