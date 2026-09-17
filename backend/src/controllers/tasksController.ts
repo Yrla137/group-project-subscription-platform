6f60ed2d1d2207c87366f679662417b36de0cd4c
SandraGEnglund <sandra.g.englund@gmail.com>
2026-09-17 11:53:10 +0200

    Implement task controller and routes for CRUD operations

---
+++ backend/src/controllers/tasksController.ts
@@ -0,0 +1,52 @@
+import * as tasksService from "../services/tasksService";
+import type { Request, Response } from "express";
+
+// GET - gets all tasks from the database
+export const getAllTasksController = async (_req: Request, res: Response) => {
+    const tasks = await tasksService.getAllTasks();
+
+    return res.status(200).json({
+        message: "Tasks fetched successfully",
+        data: tasks
+    });
+};
+
+// GET id - gets a task with a specific id from the database
+export const getTaskByIdController = async (req: Request, res: Response) => {
+    const id = Number(req.params.id);
+    const task = await tasksService.getTaskById(id);
+    if(!task){
+        return res.status(404).json({message: "Task not found"});
+    }
+        return res.json(task);
+};
+
+// PATCH - update task information
+export const updateTaskController = async (req: Request, res: Response) => {
+    const id = Number(req.params.id);
+    const updatedTasks = await tasksService.updateTask(
+        id,
+        req.body
+    );
+
+    if (!updatedTasks) {
+        return res.status(404).json({
+            message: "Tasks could not be updated"
+        });
+    }
+
+    return res.status(200).json({
+        message: "Tasks updated successfully"
+    });
+};
+
+// DELETE - delete a task
+export const deleteTaskController = async (req: Request, res: Response) => {
+    const id = Number(req.params.id);
+    const tasks = await tasksService.getTaskById(id);
+    if(!tasks){
+        return res.status(404).json({message: "Tasks not found"});
+    }
+    await tasksService.deleteTask(id);
+    return res.status(200).json({message: "Task deleted successfully"});
+};
---
+++ backend/src/routes/tasksRoutes.ts
@@ -0,0 +1,26 @@
+import express from "express";
+// import requireAuth from "../middlewares/requireAuth";
+// import requireAdmin from "../middlewares/requireAdmin";
+
+import {
+    getAllTasksController,
+    getTaskByIdController,
+    updateTaskController,
+    deleteTaskController}
+    from "../controllers/tasksController";
+
+const router = express.Router();
+
+// GET - gets all tasks from the database
+router.get("/", getAllTasksController);
+
+// GET id - gets a task with a specific id from the database 
+router.get("/:id", getTaskByIdController);
+
+// PATCH - update task information
+router.patch("/:id", updateTaskController);
+
+// DELETE - delete a task
+router.delete("/:id", deleteTaskController);
+
+export default router;
