f73462ac3c1237388473fde239a6965e8e0b80b9
Sofie Johnsson <a.e.sofie.o@gmail.com>
2026-09-17 15:04:45 +0200

    Adding tier_level to getAllSeminars

--- backend/src/services/seminarsService.ts
+++ backend/src/services/seminarsService.ts
@@ -17,9 +17,16 @@ const createSeminar = async (data: CreateSeminar): Promise<Seminar> => {
 
 // GET - gets all seminars from the database
 const getAllSeminars = async (): Promise<Seminar[]> => {
-    const result = await pool.query('SELECT id, seminar_title, seminar_description, seminar_date, tier_id, created_by, created_at FROM seminars');
+    const result = await pool.query(`
+        SELECT
+            seminars.*,
+            tiers.level AS tier_level
+        FROM seminars
+        JOIN tiers ON seminars.tier_id = tiers.id
+    `);
     return result.rows;
 }
+
 
 // GET id - gets a seminar with a specific id from the database
 const getSeminarById = async (id: number): Promise<Seminar | null> => {
