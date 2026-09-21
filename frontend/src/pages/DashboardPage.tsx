18a7a7e9584c4f7664ecd8f21ad6ea6cdbcdb769
SandraGEnglund <sandra.g.englund@gmail.com>
2026-09-21 17:04:26 +0200

    feat: integrate tasks display in calendar and update task component styling

--- frontend/src/pages/DashboardPage.tsx
+++ frontend/src/pages/DashboardPage.tsx
@@ -1,11 +1,9 @@
 import Calendar from '../components/Calendar'
-import TaskView from '../components/Tasks'
 
 const DashboardPage = () => {
   return (
     <div>
       <Calendar />
-      <TaskView />
     </div>
   )
 }
