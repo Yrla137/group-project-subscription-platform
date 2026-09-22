d10db13ca454a89f7c2bbdc0d04011506751e211
Yrla137 <julia.bjorling@hotmail.com>
2026-09-22 13:29:38 +0200

    feat: add admin pages and update routing for admin functionalities

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,6 +8,12 @@ import MembershipPage from "./pages/MembershipPage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 
+import AdminPage from "./pages/Admin/AdminPage";
+import PaymentsHistoryList from "./pages/Admin/PaymentsHistoryList";
+import TiersList from "./pages/Admin/TiersList";
+import UsersList from "./pages/Admin/UsersList";
+import CheckoutPage from "./pages/CheckoutPage";
+
 
 function App() {
   return (
@@ -19,8 +25,16 @@ function App() {
           <Route path="seminars/manage" element={<ManageSeminarsPage />} />
           <Route path="habits" element={<HabitsPage />} />
           <Route path="membership" element={<MembershipPage />} />
+
           <Route path="login" element={<LoginPage />} />
           <Route path="register" element={<RegisterPage />} />
+
+          <Route path="admin" element={<AdminPage />} />
+          <Route path="admin/payments-history" element={<PaymentsHistoryList />} />
+          <Route path="admin/tiers" element={<TiersList />} />
+          <Route path="admin/users" element={<UsersList />} />
+
+          <Route path="checkout" element={<CheckoutPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ frontend/src/pages/Admin/PaymentsHistoryList.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PaymentsHistoryList = () => {
+  return (
+    <div>PaymentsHistoryList</div>
+  )
+}
+
+export default PaymentsHistoryList
