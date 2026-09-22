9586bb9d9bbdeef1ab8c142caa5fcb7ccac0fe42
SandraGEnglund <sandra.g.englund@gmail.com>
2026-09-22 09:49:41 +0200

    bugfix: update SQL queries to return specific task fields instead of all columns for the tasks to show up on the right date in calendar.

--- backend/src/services/tasksService.ts
+++ backend/src/services/tasksService.ts
@@ -2,19 +2,19 @@ import { pool } from "../config/db";
 import type { CreateTask, UpdateTask } from "../types/tasks-type";
 
 export const getAllTasks = async () => {
- const result = await pool.query("SELECT * FROM tasks");
+ const result = await pool.query("SELECT id, user_id, task_title, task_description, TO_CHAR(task_date, 'YYYY-MM-DD') AS task_date, is_completed FROM tasks");
  return result.rows;   
 };
 
 export const getTaskById = async (id: number) => {
-    const result = await pool.query("SELECT * FROM tasks WHERE id = $1", [id]);
+    const result = await pool.query("SELECT id, user_id, task_title, task_description, TO_CHAR(task_date, 'YYYY-MM-DD') AS task_date, is_completed FROM tasks WHERE id = $1", [id]);
     return result.rows[0];
 };
 
 export const createTask = async (taskData: CreateTask) => {
     const { user_id, task_title, task_description, task_date, is_completed } = taskData;
     const result = await pool.query(
-        "INSERT INTO tasks (user_id, task_title, task_description, task_date, is_completed) VALUES ($1, $2, $3, $4, $5) RETURNING *",
+        "INSERT INTO tasks (user_id, task_title, task_description, task_date, is_completed) VALUES ($1, $2, $3, $4, $5) RETURNING id, user_id, task_title, task_description, TO_CHAR(task_date, 'YYYY-MM-DD') AS task_date, is_completed",
         [user_id, task_title, task_description || null, task_date, is_completed || false]
     );
     return result.rows[0];
@@ -23,7 +23,7 @@ export const createTask = async (taskData: CreateTask) => {
 export const updateTask = async (id: number, taskData: UpdateTask) => {
     const { task_title, task_description, task_date, is_completed } = taskData;
     const result = await pool.query(
-        "UPDATE tasks SET task_title= COALESCE($1, task_title), task_description = COALESCE($2, task_description), task_date = COALESCE($3, task_date), is_completed = COALESCE($4, is_completed) WHERE id = $5 RETURNING *",
+        "UPDATE tasks SET task_title= COALESCE($1, task_title), task_description = COALESCE($2, task_description), task_date = COALESCE($3, task_date), is_completed = COALESCE($4, is_completed) WHERE id = $5 RETURNING id, user_id, task_title, task_description, TO_CHAR(task_date, 'YYYY-MM-DD') AS task_date, is_completed",
         [task_title, task_description, task_date, is_completed, id]
     );
     return result.rows[0];
