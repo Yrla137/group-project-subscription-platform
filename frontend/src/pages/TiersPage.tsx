b02ff3d1c2665fbf80cbff34bb8ad9e9f058a4b9
Yrla137 <julia.bjorling@hotmail.com>
2026-09-24 14:16:57 +0200

    refactor: comment out unused imports and code in TiersPage component

--- frontend/src/pages/TiersPage.tsx
+++ frontend/src/pages/TiersPage.tsx
@@ -1,11 +1,11 @@
-import { usestate, useEffect } from 'react'
-import { useTiers } from '../hooks/useTiers';
-import { useAuthContext } from '../context/AuthContext';
-import type { Tier } from '../types/TierType';
+// import { usestate, useEffect } from 'react'
+// import { useTiers } from '../hooks/useTiers';
+// import { useAuthContext } from '../context/AuthContext';
+// import type { Tier } from '../types/TierType';
 
 const TiersPage = () => {
 
-  const { tiers, error, isLoading, fetchTiers } = useTiers();
+  // const { tiers, error, isLoading, fetchTiers } = useTiers();
 
   return (
     <div>
